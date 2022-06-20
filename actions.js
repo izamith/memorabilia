d0c61f322441530bd1e37c35f38de38db35f92d5
Isabela Zamith <izamith@yahoo.com.br>
2022-06-19 21:31:06 -0300

    esqueleto inicial

--- actions.js
+++ actions.js
@@ -1,1 +1,38 @@
-console.log('js funcionando!')
+var dias = [];
+
+function setup() {
+    var cv=createCanvas(displayWidth-64, displayHeight);
+    cv.parent("main");
+    for (var i = 0; i<30; i++) {
+        dias.push(new Dia(i,10+i,10+i*2));
+    }
+}
+
+function draw() {
+    background('#fff');
+    showDays();
+}
+
+
+function showDays() {
+    for (var i=0; i<dias.length; i++) {
+        dias[i].display();
+    }
+}
+
+
+// dia class
+class Dia {
+    constructor(data,locx,locy) {
+      this.data = data;
+      //this.sorrisos = sorrisos;
+      this.size = 50;
+      this.locx = locx;
+      this.locy = locy;
+    }
+  
+    display() {
+      fill('#000');
+      rect(this.locx, this.locy, this.size, this.size);
+    }
+  }
